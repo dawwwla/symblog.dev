76a2f44a0a070a00a76b2f851942e5289a50c4b5
ROYCE WHELAN <royce.whelan@gmail.com>
2013-08-11 22:55:23 -0400

    symblog part 2 complete

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -3,6 +3,12 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetaData;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\MinLength;
+use Symfony\Component\Validator\Constraints\MaxLength;
+
 class Enquiry
 {
     protected $name;
@@ -10,6 +16,20 @@ class Enquiry
     protected $subject;
     protected $body;
 
+    public static function loadValidatorMetaData(ClassMetaData $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email(array(
+                'message' => 'Symblog does not like invalid emails.  How about a real one?'
+        )));
+        
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('subject', new MaxLength(50));
+        
+        $metadata->addPropertyConstraint('body', new MinLength(50));
+    }
+    
     public function getName()
     {
         return $this->name;
